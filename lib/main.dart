6832ac7184443e30b5c352bfc364ae8557b71693
Fares443 <Fm#55555>
2024-08-11 19:14:25 +0300

    3

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'b1-m2',
+      title: '1-2',
       theme: ThemeData(
       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
