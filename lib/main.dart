e49720fd57bebaa7730f08b11b4312fec0d3edb7
Fares443 <Fm#55555>
2024-08-11 17:53:21 +0300

    m5

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'm5',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -57,7 +43,7 @@ class MyHomePage extends StatelessWidget {
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-              const Text(
+               Text(
                 'You have pushed the button this many times:',
               ),
               BlocBuilder<FeatureCubit, FeatureState>(
@@ -81,17 +67,11 @@ class MyHomePage extends StatelessWidget {
                     ;
               }),
             ])),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+       
+        // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
 }
-//m11
-//m22
-//m33
-//m44
 
+//m5
